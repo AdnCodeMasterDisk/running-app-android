d1b517f2a6398edc2ba7a3adb697ed8b0c225036
Michał Gładysz <74502510+michalgladysz@users.noreply.github.com>
2021-11-20 16:50:24 +0100

    feat: update HomeScreen with basic components

--- app/src/main/java/com/runningapp/app/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/runningapp/app/ui/screens/HomeScreen.kt
@@ -1,21 +1,27 @@
 package com.runningapp.app.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.runningapp.app.R
 import com.runningapp.app.ui.components.ActivityCard
 import com.runningapp.app.ui.components.MonthlyGoalComponent
 import com.runningapp.app.ui.utils.SimpleListDataItem
@@ -27,14 +33,48 @@ fun HomeScreen() {
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
     ) {
-        Text(
-            text = "Home View",
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colorScheme.primaryContainer,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 25.sp
-        )
+        MonthlyGoalComponent()
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Box(modifier = Modifier
+            .height(400.dp)
+            .fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.small_map),
+                contentDescription = "Activity map preview",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .height(350.dp)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(0.dp, 0.dp, 40.dp, 40.dp))
+            )
+
+            /* TO DO: real map goes here */
+
+//            Surface(modifier = Modifier
+//                .height(330.dp)
+//                .fillMaxWidth(),
+//                color = MaterialTheme.colorScheme.surfaceVariant
+//            ) {
+//                Text("To do map")
+//            }
+            Button(
+                onClick = { },
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .size(100.dp),
+                shape = CircleShape
+            ) {
+                Text(
+                    text = "GO!",
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic
+                )
+            }
+        }
     }
 }
 
